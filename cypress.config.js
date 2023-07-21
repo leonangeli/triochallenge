b05923c140d01914585eee43efc414243e54846c
aleonangeli <leonangeli.agustin@gmail.com>
2023-07-21 12:38:35 -0300

    Mochawesome reporting plugin installed

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '9enbzh',
+  reporter: 'cypress-mochawesome-reporter', //html reports
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
